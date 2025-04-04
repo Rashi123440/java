abd647110754b30f549ad0ca682146d8ec44e0c9
Rashi123440 <1811.rashi@gmail.com>
2025-04-04 19:17:23 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,36 @@
+// Superclass
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+// Subclass 1
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks.");
+    }
+}
+
+// Subclass 2
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows.");
+    }
+}
+
+// Main class
+public class main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        
+        myDog.eat();
+        myDog.bark();
+
+    
+        myCat.eat();
+        myCat.meow();
+    }
+}
